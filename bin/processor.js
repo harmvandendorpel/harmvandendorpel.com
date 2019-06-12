d71428af8c80c0df52152ceae96f8a0d8ef5fd7b
Use vh <>
2019-06-12 13:13:37 +0200

    current

--- bin/processor.js
+++ bin/processor.js
@@ -1,5 +1,4 @@
 const FILENAME = './_/work.json'
-<<<<<<< HEAD
 
 main()
 
@@ -33,27 +32,3 @@ function processItem(item) {
   
   return newItem
 }
-=======
-const fs = require('fs')
-const work = JSON.parse(fs.readFileSync(FILENAME, 'utf8'))
-
-const result = {
-  content: work.content.map(item => {
-    const newItem = {
-      ...item,
-      date: new Date(item.date)
-    }
-
-    if ('pubDate' in newItem) {
-      newItem.pubDate = new Date(newItem.pubDate)
-    } else {
-      newItem.pubDate = newItem.date
-    }
-    
-    return newItem
-  })
-}
-
-const JSONString = JSON.stringify(result, null, 2)
-fs.writeFileSync(FILENAME, JSONString)
->>>>>>> b18403d14431c02ac6889c6295e8413a3b6b9fce
