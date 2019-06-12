159893a6b238efdcd261120914a5ebf3009545c5
Use vh <>
2019-06-12 13:55:56 +0200

    publish

--- _/template.php
+++ _/template.php
@@ -222,31 +222,12 @@ function indexImage($content, $isUpcoming = false) {
 }
 
 function d($s) {
-    $m = array(
-        1 => 'January',
-        2 => 'February',
-        3 => 'March',
-        4 => 'April',
-        5 => 'May',
-        6 => 'June',
-        7 => 'July',
-        8 => 'August',
-        9 => 'September',
-        10 => 'October',
-        11 => 'November',
-        12 => 'December',
-    );
-
     $date = new DateTime($s);
-
-    // $month = $date->format('M');
-    // $arr = explode('-', $s);
-    // $monthIndex = (int)trim($arr[1]);
 
     if (isUpcoming($s)) {
         $result = $result = $date->format('j F Y'). ' (upcoming)';
     } else {
-        $result = $result = $date->format('F Y'); // $m[$monthIndex].' '. $arr[0];
+        $result = $result = $date->format('F Y');
     }
     return $result;
 }
