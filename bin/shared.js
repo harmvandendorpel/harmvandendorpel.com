3eb806dab15ddd2d5260353e6609c658dfd823f1
Use vh <>
2019-06-12 13:27:46 +0200

    publish

---
+++ bin/shared.js
@@ -0,0 +1,30 @@
+const FILENAME = './_/work.json'
+
+const fs = require('fs')
+
+function readItems() {
+  return JSON.parse(fs.readFileSync(FILENAME, 'utf8'))
+}
+
+function saveItems(result) {
+  const JSONString = JSON.stringify(result, null, 2)
+  fs.writeFileSync(FILENAME, JSONString)
+}
+
+function makePermalink(str) {
+  var re = /[^a-z0-9]+/gi; // global and case insensitive matching of non-char/non-numeric
+  var re2 = /^-*|-*$/g;     // get rid of any leading/trailing dashes
+  str = str.replace(re, '-');  // perform the 1st regexp
+  return str.replace(re2, '').toLowerCase(); // ..aaand the second + return lowercased result
+}
+
+function exists(key, array) {
+  return key in array && array[key] !== null
+}
+
+function missing(key, array) { return !exists(key, array) }
+
+
+module.exports = {
+  readItems, saveItems, makePermalink, missing, exists
+}
