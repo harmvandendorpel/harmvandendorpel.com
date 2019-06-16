66e6d672351edecc7f46b34e5b797119e2f6005e
root <root@love.harmvandendorpel.com>
2019-06-16 09:58:52 +0000

    fis

--- _/log.inc.php
+++ _/log.inc.php
@@ -7,7 +7,11 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 function logPageView() {
   global $conn;
-  $referer = $_SERVER['HTTP_REFERER'];
+  if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+    $referer = $_SERVER['HTTP_REFERER'];
+  } else {
+    $referer = ''; 
+  }	  
   $hostname = $_SERVER['REMOTE_ADDR'];
   $url = $_SERVER['REQUEST_URI'];
   $sql = "INSERT INTO logging (host,referer, url) VALUES ('$hostname','$referer','$url')";
