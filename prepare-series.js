41997392cfc4a374d8fa0624cc5bf651c2a4f6ee
harm <harm@harm.fritz.box>
2019-11-05 20:07:15 +0100

    meta images

--- prepare-series.js
+++ prepare-series.js
@@ -6,34 +6,25 @@ const rawdata = fs.readFileSync('_/work.json')
 const content = JSON.parse(rawdata)
 
 const series = {}
-content.content.forEach((item) => {
-  if (!('parts' in item)) return
-  item.parts.forEach(part => {
-    if (!('series' in part)) return
-    const seriesName = part.series
-    if (seriesName in series) {
-      series[seriesName].push(part)
-    } else {
-      series[seriesName] = [part]
+
+const result = {
+  index: content.index,
+  content: content.content.map((item) => {
+    if (!('meta_image' in item)) {
+      // item.meta_image = 'test.jpg'
+      if ('parts' in item) {
+        if (item.parts.length) {
+          if ('content' in item.parts[0]) {
+            item.meta_image = item.parts[0].content[0].filename
+
+          }
+        }
+      }
     }
+    return item
   })
-})
+}
 
-console.log(series)
+const stringData = JSON.stringify(result)
 
-content.content = content.content.map((item) => {
-  if (!('series' in item)) return item
-  
-  const parts = item.parts || []
-
-  return {
-    ...item,
-    parts: [
-      ...parts,
-      { thumbs: series[item.series] }
-    ]
-  }
-})
-
-const stringData = JSON.stringify(content)
 fs.writeFileSync('_/work-output.json', stringData)
